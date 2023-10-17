5ace1ac9f5b09d3dbd2a41b37347c9b8cc37ad32
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-17 16:26:54 -0300

    adjusting verifyToken in authService

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import AuthController from "../controllers/auth/authController.js";
+import AuthService from "../services/authService.js";
 
 const authRouter = express.Router();
 const authController = new AuthController();
+const authService = new AuthService();
 
 authRouter.post("/login", authController.login);
-authRouter.post("/logout", authController.logout);
+authRouter.post("/logout", authService.verifyToken ,authController.logout);
 
 export default authRouter;
--- src/services/authService.js
+++ src/services/authService.js
@@ -23,4 +23,4 @@ class AuthService {
   }
 }
 
-export default new AuthService();
+export default AuthService;
